0431bd51a416b043cd0f9da15f3235c591c5e1fb
{Yuki521} <{uw.y.o5o21@gmail.com}>
2021-07-11 23:09:47 +0900

    メニュー

--- Main.php
+++ Main.php
@@ -1,15 +1,14 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Menu\Recipe;
+use Uyu\Recipe;
 
-require_once __DIR__ . '/vendor/autoload.php';
 
 $recipe1 = new Recipe("ハンバーグ", 200.5);
 $recipe2 = new Recipe("サーモン", 120);
 
 echo $recipe1->getName(), PHP_EOL; // ハンバーグ
-// echo $recipe1, PHP_EOL; // 200.5
+// echo $recipe1->, PHP_EOL; // 200.5
 
 // echo $recipe2, PHP_EOL; // ハンバーグ
 // echo $recipe2, PHP_EOL; // 200.5
--- src/Recipe.php
+++ src/Recipe.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Menu;
+namespace Uyu;
 
 class Recipe
 {
