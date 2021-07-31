98260ff160f905c6031455182c783a1998ce3ef2
{Yuki521} <{uw.y.o5o21@gmail.com}>
2021-07-31 11:05:36 +0900

    clear 7

--- src/Algorithm/CalorieOverSpec.php
+++ src/Algorithm/CalorieOverSpec.php
@@ -7,8 +7,27 @@ use Uyu\MenuSpec;
 
 class CalorieOverSpec implements MenuSpec
 {
-  public function satisfiedBy(Menu $menu): bool
-  {
-    return false;
-  }
+
+    private int $cal;
+
+    /**
+     * constructer
+     *
+     * @param integer $cal
+     */
+    public function __construct(int $cal)
+    {
+        $this->cal = $cal;
+    }
+
+    /**
+     * メニューの合計カロリーが$calよりも多ければtrueを返します。
+     *
+     * @param Menu $menu
+     * @return boolean
+     */
+    public function satisfiedBy(Menu $menu): bool
+    {
+        return $menu->getCal() >= $this->cal;
+    }
 }
--- src/Algorithm/MenuTypeSpec.php
+++ src/Algorithm/MenuTypeSpec.php
@@ -7,13 +7,25 @@ use Uyu\MenuSpec;
 
 class MenuTypeSpec implements MenuSpec
 {
+
     private string $type;
 
+    /**
+     * constructer
+     *
+     * @param [type] $type
+     */
     public function __construct($type)
     {
         $this->type = $type;
     }
 
+    /**
+     * menuのタイプが$typeと同じならtrueを返します。
+     *
+     * @param Menu $menu
+     * @return boolean
+     */
     public function satisfiedBy(Menu $menu): bool
     {
         return $menu->getType() == $this->type;
