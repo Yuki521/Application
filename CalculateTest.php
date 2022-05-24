2aa0f38cebce2e509ab4b774a99836f4251fe77c
{Yuki521} <{uw.y.o5o21@gmail.com}>
2022-05-24 22:16:53 +0900

    add test

---
+++ CalculateTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Yuki\lesson2;
+
+use PHPUnit\Framework\TestCase;
+
+class CalculateTest extends TestCase
+{
+
+
+    /**
+     * @dataProvider CalcProvider
+     */
+    public function testCalc($stdin, $expected)
+    {
+        $formula = explode(' ', $stdin);
+        $calculate = new Calculate($formula[0]);
+        $actual = $calculate->calc($formula[1], $formula[2]);
+
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function CalcProvider()
+    {
+        return [
+            '+' => ['+ 1 3', 4],
+            '-' => ['- 1 3', -2],
+            'm' => ['m 1 3', 3],
+            '/' => ['/ 100 2', 50],
+            '%' => ['% 10 3', 1],
+        ];
+
+    }
+}
